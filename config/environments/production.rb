524c05e3d8a2e5d347b9700c16397b09ddc362bf
Jon Kessler <jwkpiano1@gmail.com>
2013-03-10 17:08:10 -0700

    improve appearance of devise views, set up smtp settings for mail sending on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,16 @@ Relect::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'https://relect.herokuapp.com' }
 
+  ActionMailer::Base.smtp_settings = {
+    port: '587',
+    address: 'smtp.mandrillapp.com',
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_APIKEY'],
+    domain: 'heroku.com',
+    authentication: :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Enable threaded mode
   # config.threadsafe!
 
