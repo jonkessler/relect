00a4166dffdbc44c0ede6dee08689fdabd2c1c9c
Jon Kessler <jwkpiano1@gmail.com>
2013-03-14 12:16:19 -0700

    handle candidates getting zero votes better

--- lib/instant_runoff_election.rb
+++ lib/instant_runoff_election.rb
@@ -1,22 +1,25 @@
 class InstantRunoffElection
-  def initialize(ballots)
+  def initialize(ballots, candidates)
     @ballots = Marshal.load(Marshal.dump(ballots))
+    @candidates = candidates.dup
   end
 
   def run!
-    #TODO: eliminate candidates with no votes in any particular round
-    
     results = nil
     rounds = []
-    loop do
+    (@candidates.size - 1).times do
       # remove empty ballots
       @ballots.reject!(&:empty?)
 
-      results = @ballots.map(&:first).group_by{|c| c}.map{|candidate, votes| [candidate, votes.size]}.sort{|a, b| b[1] <=> a[1]}
+      first_preferences = @ballots.map(&:first)
+      results = @candidates.map{|c| [c, first_preferences.select{|pref| c == pref}.size]}.sort{|a, b| b[1] <=> a[1]}
       rounds << results.dup
       break if results.size <= 2 || results.first[1] >= (@ballots.size / 2.0).ceil
 
       eliminated_candidate = results.last[0]
+      @candidates -= [eliminated_candidate]
+      # @candidates.reject!{|c| c == eliminated_candidate}
+      
       @ballots.each do |ballot|
         ballot.reject!{|vote| vote == eliminated_candidate}
       end
