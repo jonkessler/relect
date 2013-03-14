ba93f4da4ce327c28120637559820f58e4fb658f
Jon Kessler <jwkpiano1@gmail.com>
2013-03-14 12:18:06 -0700

    refactoring

--- lib/instant_runoff_election.rb
+++ lib/instant_runoff_election.rb
@@ -11,14 +11,12 @@ class InstantRunoffElection
       # remove empty ballots
       @ballots.reject!(&:empty?)
 
-      first_preferences = @ballots.map(&:first)
-      results = @candidates.map{|c| [c, first_preferences.select{|pref| c == pref}.size]}.sort{|a, b| b[1] <=> a[1]}
+      results = @candidates.map{|c| [c, @ballots.map(&:first).select{|pref| c == pref}.size]}.sort{|a, b| b[1] <=> a[1]}
       rounds << results.dup
       break if results.size <= 2 || results.first[1] >= (@ballots.size / 2.0).ceil
 
       eliminated_candidate = results.last[0]
       @candidates -= [eliminated_candidate]
-      # @candidates.reject!{|c| c == eliminated_candidate}
       
       @ballots.each do |ballot|
         ballot.reject!{|vote| vote == eliminated_candidate}
