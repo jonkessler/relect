7caca427f8b931bf14a1660fb0f2eb4316fe233a
Jon Kessler <jwkpiano1@gmail.com>
2016-03-04 10:51:27 -0800

    wip

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,18 @@
 module Users
   class SessionsController < Devise::SessionsController
     skip_authorization_check
+
+    def create
+      if (token = params[:token])
+        begin
+        response = RestClient::Resource.new('https://www.googleapis.com/oauth2/v3/tokeninfo').post(id_token: token)
+        response
+        rescue RestClient::Exception
+          super
+        end
+      else
+        super
+      end
+    end
   end
 end
